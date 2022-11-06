7b23b0e8888722b4b916e112d57bdac5c796b1fd
Savoi1234 <112000562+Savoi1234@users.noreply.github.com>
2022-11-05 20:00:54 -0400

    Java Input prompt

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,3 +1,15 @@
 window.onload = () => {
     alert(`JavaScript Works`);
-};
+let col = 10; 
+let row = 10; 
+
+}
+window.onload = () =>{ 
+    let num = ''; 
+    let counter = 1; 
+
+
+    let input = window.prompt('Input Number'); 
+
+
+}
